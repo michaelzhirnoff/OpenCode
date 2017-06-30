2d26561a47c67db6a87add29b929520037bab1cf
michaelzhirnoff <firemikhail@gmail.com>
2017-06-30 10:23:15 +0400

    Default access

--- opencode/Factorial.java
+++ opencode/Factorial.java
@@ -6,11 +6,11 @@ public class Factorial {
         Scanner factorialScanner = new Scanner(System.in);
         System.out.println("Add any number from 0 to 99");
         int number = factorialScanner.nextInt();
-        long factorial = 1;
+        long result = 1;
         int i;
         for (i = number; i > 0; i--) {
-            factorial *= i;
+            result *= i;
         }
-        System.out.format("Factorial of %d equals %d\n", number, factorial);
+        System.out.format("Factorial of %d equals %d\n", number, result);
     }
 }
