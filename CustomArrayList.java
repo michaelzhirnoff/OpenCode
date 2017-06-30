acc2f3497a729780185447dd82fedb292273bab0
zhirnoff <firemikhail@gmail.com>
2017-06-30 10:15:55 +0400

    1

---
+++ CustomArrayList.java
@@ -0,0 +1,12 @@
+package opencode;
+
+class CustomArrayList {
+        private static final int arraySize = 10;
+        private Object[] objectArray;
+        private int currentSize;
+
+        public CustomArrayList() {
+            // initiliazes elements
+            
+    }
+}
---
+++ Factorial.java
@@ -0,0 +1,16 @@
+package opencode;
+import java.util.Scanner;
+
+public class Factorial {
+    public static void main(String[] args) {
+        Scanner factorialScanner = new Scanner(System.in);
+        System.out.println("Add any number from 0 to 99");
+        int number = factorialScanner.nextInt();
+        long factorial = 1;
+        int i;
+        for (i = number; i > 0; i--) {
+            factorial *= i;
+        }
+        System.out.format("Factorial of %d equals %d\n", number, factorial);
+    }
+}
